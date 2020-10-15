1d6ad18eb7e9f3756f74bb6fa10f15297bc33d95
Ferrin Katz <ferrinkatz@ferrinkatz.com>
2020-10-14 22:22:42 -0700

    Add connector to send messages to kafka, and docker-compose.yml for local kafka cluster for testing.

--- src/main/kotlin/org/example/Orders.kt
+++ src/main/kotlin/org/example/Orders.kt
@@ -1,15 +1,36 @@
 package org.example
 
-import kotlinx.coroutines.*
+import com.fasterxml.jackson.core.io.JsonStringEncoder
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import kafka.utils.json.JsonObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.apache.kafka.clients.producer.*
+import org.apache.kafka.common.serialization.StringSerializer
+import org.json.JSONObject
+import java.net.InetAddress
+import java.util.*
+import java.util.concurrent.Future
 import kotlin.math.floor
+
 
 sealed class Orders
 class NewOrder(val items: List<String>) : Orders()
 class SetStock(val apples: Int, val oranges: Int) : Orders()
+
+data class OrderSubmitted(
+    val Message: String
+)
 
 fun CoroutineScope.ordersActor(mail: SendChannel<String>) = actor<Orders> {
     val stock: MutableMap<String, Int> = mutableMapOf()
@@ -22,21 +43,21 @@ fun CoroutineScope.ordersActor(mail: SendChannel<String>) = actor<Orders> {
                     when (it) {
                         "apple" -> ++numApples
                         "orange" -> ++numOranges
-                        else -> {}
+                        else -> {
+                        }
                     }
                 }
 
                 val outputString = StringBuilder()
                 var applesRemaining = stock.getOrDefault("apples", 0)
                 var orangesRemaining = stock.getOrDefault("oranges", 0)
-                if(numApples < applesRemaining && numOranges < orangesRemaining) {
+                if (numApples < applesRemaining && numOranges < orangesRemaining) {
                     outputString.append("The total is " + getTotal(numApples, numOranges))
                     applesRemaining -= numApples
                     orangesRemaining -= numOranges
                     stock["apples"] = applesRemaining
                     stock["oranges"] = orangesRemaining
-                }
-                else {
+                } else {
                     outputString.append("Error: ")
 
                     if (numApples > applesRemaining)
@@ -64,12 +85,35 @@ fun getTotal(apples: Int, oranges: Int): Int {
     return total
 }
 
+private fun createProducer(brokers: String): Producer<String, String> {
+    val props = Properties()
+    props["bootstrap.servers"] = brokers
+    props["client.id"] = InetAddress.getLocalHost().getHostName()
+    props["acks"] = "all"
+    props["key.serializer"] = StringSerializer::class.java.canonicalName
+    props["value.serializer"] = StringSerializer::class.java.canonicalName
+    props["key.converter.schema.registry.url"] = "http://localhost:8081"
+    props["value.converter.schema.registry.url"] = "http://localhost:8081"
+    props["schema.registry.url"] = "http://localhost:8081"
+    return KafkaProducer<String, String>(props)
+}
+
 fun main(): Unit = runBlocking {
+    val producer = createProducer("localhost:9092")
+    val jsonMapper = ObjectMapper().apply {
+        registerKotlinModule()
+        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        setDateFormat(StdDateFormat())
+    }
+
+
     val mail = Channel<String>()
 
     GlobalScope.launch {
         mail.consumeEach {
-            println(it)
+            val orderMsg = OrderSubmitted(it)
+            val future: Future<RecordMetadata> = producer.send(ProducerRecord("order-submitted", jsonMapper.writeValueAsString(orderMsg)))
+            val metadata = future.get()
         }
     }
 
